1b1ec0592c013eecae8eb5d951ebb9bd221e420b
Johannes Filter <hi@jfilter.de>
2017-12-13 13:17:04 +0100

    add proper concatenation

--- coreg/TrainConvAutoencoder.py
+++ coreg/TrainConvAutoencoder.py
@@ -3,10 +3,11 @@ import numpy as np
 from skimage import io
 from pathlib import Path
 
-from chungcore.ConvAutoencoder import ConvAutoEncoder2D
-from chungcore.MakeDataset import save_array, normalize, load_array
+from ConvAutoencoder import ConvAutoEncoder2D
+from MakeDataset import save_array, normalize, load_array
 import config
 
+print(config)
 
 def get_files(dir):
     for root, dirs, files in os.walk(config.image_1_dir):
@@ -15,21 +16,36 @@ def get_files(dir):
         return files
 
 
-def concatenate_images(image_1_filename, image_2_filename):
-    image1 = io.readData(Image1Directory)
-    image2 = io.readData(Image2Directory)
+def concatenate_images(image_1_filename, image_2_filename, i):
+    image1 = io.imread(image_1_filename)
+    image2 = io.imread(image_2_filename)
     print(image1.shape, image2.shape)
     image = np.concatenate((image1, image2), axis=2)
     image = np.swapaxes(image, 1, 2)
     image = np.swapaxes(image, 0, 1)
     X = np.expand_dims(image, axis=3)
-    save_array(ProcessedDirectory, X)
+    return image
 
 # image 1
 images_1 = get_files(config.image_1_dir)
 images_2 = get_files(config.image_2_dir)
 
-for i in range(len(images_1)):
+concatenated_filename = config.processed_dir + '/concatenated'
+
+if os.path.isfile(concatenated_filename):
+    X = load_array(concatenated_filename)
+else:
+    X = []
+    for i in range(len(images_1)):
+        X.append(
+            concatenate_images(images_1[i], images_2[i], i))
+    save_array(concatenated_filename, X)
+
+
+# Train
+mynet = ConvAutoEncoder2D(config.checkpoint_dir +
+                          'mynet', X.shape[1], X.shape[2])
+mynet.train(X)
 
 
 # # Prepare data
@@ -44,8 +60,4 @@ for i in range(len(images_1)):
 #     X = np.expand_dims(image, axis=3)
 #     save_array(ProcessedDirectory, X)
 # else:
-#     X = load_array(ProcessedDirectory)
 
-# # Train
-# mynet = ConvAutoEncoder2D(ModelFile, X.shape[1], X.shape[2])
-# mynet.train(X)
--- coreg/config.py
+++ coreg/config.py
@@ -1,4 +1,5 @@
-image_1_dir = 'data/mnist_min/0_1'
-image_2_dir = 'data/mnist_min/0_2'
-processed_dir = 'data/processed'
-checkpoint_dir = 'checkpoints'
+class config:
+    image_1_dir = 'data/mnist_min/0_1'
+    image_2_dir = 'data/mnist_min/0_2'
+    processed_dir = 'data/processed'
+    checkpoint_dir = 'checkpoints'
