ed3e8690895318fbeb1b33e66214b8e8b1910a76
Johannes Filter <hi@jfilter.de>
2018-01-08 13:30:14 +0100

    use path from config, create dir if not existent

--- coreg/TrainConvAutoencoder.py
+++ coreg/TrainConvAutoencoder.py
@@ -6,9 +6,6 @@ from ConvAutoencoder import ConvAutoEncoder2D
 from MakeDataset import save_array, normalize, load_array
 import config
 
-ModelFileAutoEncoder = r'models/coreg_autoencoder.hdf5'
-
-
 def get_files(dir):
     for root, dirs, files in os.walk(config.image_1_dir):
         return [os.path.join(config.image_1_dir, fn) for fn in files]
@@ -54,11 +51,15 @@ else:
 
 print(X.shape)
 
-#checkpoint_filename = os.path.join(config.checkpoint_dir, 'mynet')
+checkpoint_filename = os.path.join(config.checkpoint_dir, 'mynet')
+
+# create checkpoints folder if needed
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
 
 # Train
 mynet = ConvAutoEncoder2D(
-    ModelFileAutoEncoder, X.shape[1], X.shape[2], config.encoding_decoding_choice)
+    checkpoint_filename, X.shape[1], X.shape[2], config.encoding_decoding_choice)
 mynet.train(X)
 
 
