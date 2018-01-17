2fa96ffa17e368fb7b844243ab9bf1ddfe64c72f
Johannes Filter <hi@jfilter.de>
2018-01-17 20:20:54 +0100

    add preprocess script

---
+++ preprocess/prepare_rotated_mnist.py
@@ -0,0 +1,27 @@
+from os import path, walk, makedirs
+from random import randint
+from skimage import io, transform
+
+def get_filenames(index_start, index_end):
+    for root, dirs, files in walk('data/raw/mnist_png/training/4'):
+      return [path.join('data/raw/mnist_png/training/4', fn) for fn in files[index_start:index_end]]
+
+
+for index, fn in enumerate(get_filenames(0, 100)):
+    image = io.imread(fn)
+    image = transform.rotate(image, randint(0, 360))
+
+    dir = 'data/mnist/training/moving/'
+    if not path.exists(dir):
+        makedirs(dir)
+    io.imsave(dir + str(index) + '.png', image)
+
+
+for index, fn in enumerate(get_filenames(100, 200)):
+    image = io.imread(fn)
+    # image = transform.rotate(image, randint(0, 360))
+
+    dir = 'data/mnist/training/fixed/'
+    if not path.exists(dir):
+        makedirs(dir)
+    io.imsave(dir + str(index) + '.png', image)
